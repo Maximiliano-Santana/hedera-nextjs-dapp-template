7761c5751a4a21d9cf9b24c593e9ae3442159bbf
Maximiliano Santana <iimaxsany@gmail.com>
2025-03-12 06:59:14 -0600

    Update using types on wallet connect client

--- src/services/wallets/wallet-connect/walletConnectClient.tsx
+++ src/services/wallets/wallet-connect/walletConnectClient.tsx
@@ -1,29 +1,28 @@
-import { DAppConnector } from "@hashgraph/hedera-wallet-connect";
+import { DAppConnector, DAppSigner } from "@hashgraph/hedera-wallet-connect";
 import { HederaSessionEvent, HederaChainId, HederaJsonRpcMethod } from "@hashgraph/hedera-wallet-connect";
-import { LedgerId, PublicKey } from "@hashgraph/sdk";
+import { LedgerId, PublicKey, Transaction, TransactionResponse } from "@hashgraph/sdk";
 import { useWalletState } from "@/services/walletState";
-import { SignClientTypes } from "@walletconnect/types";
+import { SessionTypes, SignClientTypes } from "@walletconnect/types";
 
 interface walletConnectClient{
     connector: DAppConnector | undefined;
-    session: any;
-    getInstance?: () => void;
-    connect?: () => void;
-    disconnect?: () => void;
-    getSigner?: () => void;
-    getAccountId?: () => void;
-    getPublicKey?: () => void;
-    signTransaction?: (tx: any) => void;
+    session: SessionTypes.Struct | undefined;
+    connect: () => Promise<void>;
+    disconnect: () => Promise<void>;
+    getSigner?: () => DAppSigner;
+    getAccountId?: () => string;
+    getPublicKey?: () => PublicKey;
+    signTransaction?: <T extends Transaction>(tx: T) => Promise<string | null>;
 }
 
 export default class WalletConnectClient implements walletConnectClient{
     private static instance: WalletConnectClient
     connector: DAppConnector | undefined;
-    session: any;
+    session: SessionTypes.Struct | undefined;
 
     constructor(){}
 
-    static async getInstance() {
+    static async getInstance(): Promise<WalletConnectClient> {
         if (!WalletConnectClient.instance) {
             const instance = new WalletConnectClient();
             await instance.init();
@@ -51,57 +50,73 @@ export default class WalletConnectClient implements walletConnectClient{
             [HederaChainId[network as keyof typeof HederaChainId]]
         );
         
-        console.log(`Initilizing WalletConnect... :${network}`);
         await this.connector.init({logger: 'error'})
-        console.log("Wallet connect initialized");
     }
 
-    async connect(){
+    async connect(): Promise<void> {
         const { connect } = useWalletState.getState();
         this.session = await this.connector?.openModal();
-        console.log(this.session);
+
+        // Validates if session was created
+        if (!this.session) {
+            console.warn('No session found. Wallet connection failed.');
+            return;
+        }
         
         //Syncronize walletStore
         const sessionAccount = this.session.namespaces?.hedera?.accounts?.[0]
         const sessionParts = sessionAccount?.split(':')
         const accountId = sessionParts.pop()
+        
+        if (!accountId) {
+            console.warn('Failed to extract accountId.');
+            return;
+        }
         connect(accountId)
     }
     
-    async disconnect(){
-        console.log('disconnecting')
+    async disconnect(): Promise<void>{
         const { disconnect } = useWalletState.getState();
     
-        if(this.session){
-            console.log(this.session)
-            this.connector?.disconnectAll().then(()=>{
-                disconnect()
-            });
+        if(this.session && this.connector){
+            await this.connector.disconnectAll()
+            disconnect();
         } 
     }
     
-    getSigner() {
-        if (this.connector?.signers.length === 0) {
+    getSigner(): DAppSigner{
+        if (!this.connector ||this.connector?.signers.length === 0) {
           throw new Error('No signers found!');
         }
         return this.connector?.signers[0];
     }
     
-    getAccountId(){
+    getAccountId(): string {
+        if(!this.session || !this.session.namespaces?.hedera?.accounts?.[0]) return '';
+
         const sessionAccount = this.session.namespaces?.hedera?.accounts?.[0]
         const sessionParts = sessionAccount?.split(':')
         const accountId = sessionParts.pop()
+
+        if (!accountId) {
+            console.warn('Failed to extract accountId.');
+            return '';
+        }
         return accountId
     }
     
-    getPublicKey(){
+    getPublicKey(): PublicKey {
+        if(!this.session){
+            throw new Error('No session found. Wallet connection failed.');           
+        }
+
         return PublicKey.fromStringED25519(this.session.peer.publicKey)
     }
     
-    async signTransaction(tx: any){
-        const signer = this.getSigner();
+    async signTransaction<T extends Transaction>(tx: T): Promise<string | null> {
+        const signer: DAppSigner = this.getSigner();
         await tx.freezeWithSigner(signer);
-        const txResult = await tx.executeWithSigner(signer);
-        return txResult ? txResult.transactionId : null;
+        const txResult: TransactionResponse = await tx.executeWithSigner(signer);
+        return txResult ? txResult.transactionId.toString() : null;
     }
 }
